a477a7adb420d6be09266da7c6c9a48926515d40
Thomas Baron <thomasjbaron@hotmail.com>
2015-05-21 01:51:34 -0600

    Switched way from tffileserver and towards connect.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 #! /usr/bin/env node
 
-var fileserver = require('tffileserver');
+var http = require('http');
+var connect = require('connect')();
+var connectStatic = require('connect-static');
+var browserify = require('browserify-middleware');
+var tfcss = require('tfcss');
 
-var server = fileserver.init(null, './www');
+var app = http.createServer(connect);
 
-fileserver.compileJavascript('./client_scripts/index.js', '/bundle.js', true, process.env.DYNO ? false : true);
-fileserver.compileCSS('./client_styles/config.json', '/bundle.css', true);
+var options = {
+	dir: 'www',
+	aliases: [
+		['/', '/index.html'],
+	],
+};
+connectStatic(options, function(err, res) {
+	tfcss.bundle('client_styles/config.json', function(err, css) {
+		connect.use('/', res);
+		connect.use('/bundle.js', browserify('client_scripts/index.js', process.env.DYNO ? browserify.settings.production : browserify.settings.development));
+		connect.use('/bundle.css', function(req, resp) {
+			resp.writeHead(200, {'Content-Type': 'text/css'});
+			resp.end(css);
+		});
+	});
+});
 
-server.listen(process.env.PORT ? Number(process.env.PORT) : 8888);
+app.listen(process.env.PORT ? Number(process.env.PORT) : 8888);
