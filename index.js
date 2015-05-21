f24efd68a0501630b90a9b720281376b3c32e87f
Thomas Baron <thomasjbaron@hotmail.com>
2015-05-21 01:26:25 -0600

    Code should now minify when we deploy to heroku.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fileserver = require('tffileserver');
 
 var server = fileserver.init(null, './www');
 
-fileserver.compileJavascript('./client_scripts/index.js', '/bundle.js', true, true);
+fileserver.compileJavascript('./client_scripts/index.js', '/bundle.js', true, process.env.DYNO ? false : true);
 fileserver.compileCSS('./client_styles/config.json', '/bundle.css', true);
 
 server.listen(process.env.PORT ? Number(process.env.PORT) : 8888);
